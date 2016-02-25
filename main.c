fb0a4f53eda06eabab1c1a8b364e0fed5f6bee01
neoHung <iamhahar@gmail.com>
2016-02-26 01:40:59 +0800

    Add i2c

--- main.c
+++ main.c
@@ -10,13 +10,34 @@ void Measure(unsigned int chan);
 bool          ADCDone;             // ADC Done flag
 unsigned int    ADCValue;                // Measured ADC Value
 void uart_rx(char data);
+
+unsigned char RXData;
+unsigned char RXCompare;
+unsigned char TXData;
+unsigned char TXByteCtr;
+
+
+void init_I2C(void) {
+      P1SEL |= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+      P1SEL2|= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+      UCB0CTL1 |= UCSWRST;                      // Enable SW reset
+      UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     // I2C Master, synchronous mode
+      UCB0CTL1 = UCSSEL_2 + UCSWRST;            // Use SMCLK, keep SW reset
+      UCB0BR0 = 12;                             // fSCL = SMCLK/12 = ~100kHz
+      UCB0BR1 = 0;
+      UCB0I2CSA = 0x069;                        // Slave Address is 048h
+      UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
+      IE2 |= UCB0RXIE;                          // Enable RX interrupt
+      RXCompare = 0;                            // Used to check incoming data
+	  TXData = 0x00;                            // Holds TX data
+}
+
 void main(void)
 {
  WDTCTL = WDTPW + WDTHOLD; // Stop WDT
 
- P1DIR |= BIT0 + BIT6; // Set the LEDs on P1.0, P1.6 as outputs
+ P1DIR |= BIT0; // Set the LEDs on P1.0, P1.6 as outputs
  //P1OUT = BIT0; // Set P1.0
- //P1OUT = BIT6;
  uart_init(uart_rx);
  //LPM0_bits
  //
@@ -25,7 +46,42 @@ void main(void)
    TA0CTL = TASSEL_2+ID_3+ MC_1;  // SMCLK, div 8, up mode,
    TACCTL0 = CCIE;        // Enable interrupts for CCR0.
  //
+//         /|\           /|\ /|\
+//          |   TMP100   10k 10k     MSP430G2xx3
+//          |   -------   |   |   -------------------
+//          +--|Vcc SDA|<-|---+->|P1.7/UCB0SDA    XIN|-
+//          |  |       |  |      |                   |
+//          +--|A1,A0  |  |      |               XOUT|-
+//             |       |  |      |                   |
+//          +--|Vss SCL|<-+------|P1.6/UCB0SCL   P1.0|---> LED
+//         \|/  -------          |                   |
+//
+
+  init_I2C();
+//Send I2C TX
+  while (1)
+  {
+    TXByteCtr = 1;                          // Load TX byte counter
+    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
+    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+                                            // Remain in LPM0 until all data
+                                            // is TX'd
+    TXData++;                               // Increment data byte
+  }
+
+  //Read I2C_RX
+  while (1)
+      {
+        while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+        UCB0CTL1 |= UCTXSTT;                    // I2C start condition
+        while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
+        UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+        __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+      }
+
  
+ /*
 while(1)
 {
 	//UARTSendArray("ABC", 3);
@@ -39,7 +95,37 @@ while(1)
 	//LPM0_bits+
 	__bis_SR_register(GIE); // Enter LPM0, interrupts enabled
 }
+*/
 }
+
+//For I2C_TX
+ __attribute__((interrupt(USCIAB0TX_VECTOR)))
+void USCIAB0TX_ISR(void)
+{
+  if (TXByteCtr)                            // Check TX byte counter
+  {
+    UCB0TXBUF = TXData;                     // Load TX buffer
+    TXByteCtr--;                            // Decrement TX byte counter
+  }
+  else
+  {
+    UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+    IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag
+    __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
+  }
+}
+
+
+/*
+//For I2C RX
+ // USCI_B0 Data ISR
+ __attribute__((interrupt(USCIAB0TX_VECTOR)))
+  void USCIAB0TX_ISR(void)
+    {
+      RXData = UCB0RXBUF;                       // Get RX data
+      __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
+    }
+*/
 
 void uart_rx(char data)
 {
@@ -57,7 +143,8 @@ void uart_rx(char data)
 __attribute__((interrupt(TIMER0_A0_VECTOR)))
 void neo_TimerA_procedure(void)
 { 
-P1OUT ^= BIT6;
+
+P1OUT ^= BIT0;
 //TA0CCR0 += 32768;    // Add offset to CCR0
 //__bic_SR_register_on_exit(CPUOFF);
 // Enable CPU so the main while loop continues
