99a6f5d92eb60b65a3a73fed54e085ee4869ba94
neoHung <iamhahar@gmail.com>
2016-03-02 01:29:41 +0800

    add soft i2c

---
+++ i2c.h
@@ -0,0 +1,47 @@
+#ifndef __I2C_H__
+#define __I2C_H__
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned long uint32_t;
+typedef unsigned long long uint64_t;
+
+#define BUFFER_LENGTH 16
+//#define CPU_F 		((double) 16000000.0)//CPU Frequency
+//#define delay_us(x) __delay_cycles((long)(CPU_F*(double)x/1000000.0))
+//#define delay_ms(x) __delay_cycles((long)(CPU_F*(double)x/1000.0))
+
+#define F_CPU 		(double)16000000L
+#define TICKS_PER_MS (F_CPU / 1000) 
+#define TICKS_PER_US (TICKS_PER_MS / 1000) 
+
+const uint8_t DELAY_LONG = 32; 
+const uint8_t DELAY_FULL = 8; 
+const uint8_t DELAY_HALF = 4; 
+const uint8_t DELAY_PART = 2; 
+
+
+//BCSCTL1 = CALBC1_16MHZ;
+//DCOCTL = CALDCO_16MHZ;
+
+
+uint8_t rxBuffer[BUFFER_LENGTH]; 
+uint8_t rxBufferIndex = 0; 
+uint8_t rxBufferLength = 0; 
+uint8_t txAddress = 0; 
+uint8_t txBuffer[BUFFER_LENGTH]; 
+uint8_t txBufferIndex = 0; 
+uint8_t txBufferLength = 0; 
+uint8_t transmitting = 0; 
+
+typedef enum {
+	INPUT = 0x00,
+	OUTPUT = 0x01,
+	HIGH = 0x01,
+	LOW = 0x00
+}PIN_MODE;
+
+
+
+#endif
+
